22ba8c71c67df0ba17462f79a01c37bab1a9000e
Roberto-Gomez1 <nuevoz9c@gmail.com>
2023-05-06 20:01:28 -0600

    prueba1

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ class login{
         this.btn = document.getElementById('btn');
         this.btn.addEventListener('click', this.validar.bind(this));
     }
-    validar(){
+    validar() {
         const data = JSON.parse(localStorage.getItem("alumnos"));
-        if (!Array.isArray(data) || data.length === 0) {
-            alert('No hay usuarios registrados');
-            return;
+      
+        if (!data) {
+          alert('No hay datos guardados en el localStorage');
+          return;
         }
+      
         console.log(JSON.stringify(data));
         const alumno = data.find((alumno) => alumno.carnet === parseInt(this.user.value) && alumno.contraseña === this.passw.value);
+      
         if(this.user.value === 'admin' && this.passw.value === 'admin'){
-            window.location = 'dashboard.html';
+          window.location = 'dashboard.html';
         }else if(alumno){
-            window.location = 'dashboardestudiante.html';
+          window.location = 'dashboardestudiante.html';
         }else{
-            alert('Usuario o contraseña incorrectos');
+          alert('Usuario o contraseña incorrectos');
         }
-    }
-    
+      }
+      
 }    
 
 const loginObj = new login();
