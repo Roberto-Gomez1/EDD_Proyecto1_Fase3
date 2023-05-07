2f6fb577c38cbeb0c3761bbaed3d177c30323c4f
Roberto-Gomez1 <nuevoz9c@gmail.com>
2023-05-07 03:00:10 -0600

    ya

--- Matriz.js
+++ Matriz.js
@@ -1,24 +1,24 @@
-class nodoMatrizAdyacencia{
-    constructor(valor){
+class nodoMatrizAdyacencia {
+    constructor(valor) {
         this.siguiente = null
         this.abajo = null
         this.valor = valor
     }
 }
 
-class grafoDirigido{
-    constructor(){
+class grafoDirigido {
+    constructor() {
         this.principal = null
     }
 
-    insertarF(texto){
+    insertarF(texto) {
         const nuevoNodo = new nodoMatrizAdyacencia(texto)
-        if(this.principal === null){
+        if (this.principal === null) {
             this.principal = nuevoNodo
-        }else{
+        } else {
             let aux = this.principal
-            while(aux.abajo){
-                if(aux.valor === nuevoNodo.valor){
+            while (aux.abajo) {
+                if (aux.valor === nuevoNodo.valor) {
                     return
                 }
                 aux = aux.abajo
@@ -27,25 +27,25 @@ class grafoDirigido{
         }
     }
 
-    insertarC(padre, hijo){
+    insertarC(padre, hijo) {
         const nuevoNodo = new nodoMatrizAdyacencia(hijo)
-        if(this.principal !== null && this.principal.valor === padre){
+        if (this.principal !== null && this.principal.valor === padre) {
             let aux = this.principal
-            while(aux.siguiente){
+            while (aux.siguiente) {
                 aux = aux.siguiente
             }
             aux.siguiente = nuevoNodo
-        }else{
+        } else {
             this.insertarF(padre)
             let aux = this.principal
-            while(aux){
-                if(aux.valor === padre){
+            while (aux) {
+                if (aux.valor === padre) {
                     break;
                 }
                 aux = aux.abajo
             }
-            if(aux !== null){
-                while(aux.siguiente){
+            if (aux !== null) {
+                while (aux.siguiente) {
                     aux = aux.siguiente
                 }
                 aux.siguiente = nuevoNodo
@@ -53,29 +53,57 @@ class grafoDirigido{
         }
     }
 
-    insertarValores(padre, hijos){
+    insertarValores(padre, hijos) {
         let cadena = hijos.split(',')
-        for(let i = 0; i < cadena.length; i++){
-            this.insertarC(padre,cadena[i])
+        for (let i = 0; i < cadena.length; i++) {
+            this.insertarC(padre, cadena[i])
         }
     }
 
+
+    buscar(valor) {
+        let arreglo = []
+        if (valor === "/") {
+            let aux = this.principal
+            while (aux) {
+                arreglo.push(aux.valor)
+                aux = aux.siguiente
+            }
+            return arreglo
+        } else {
+            let aux = this.principal
+            while (aux) {
+                if (aux.valor === valor) {
+                    aux = aux.siguiente
+                    while (aux) {
+                        arreglo.push(aux.valor)
+                        aux = aux.siguiente
+                    }
+                    return arreglo
+                }
+                aux = aux.abajo
+            }
+        }
+
+
+    }
+
     //Reporte modificado para trabajar con carpetas
-    grafica(){
+    grafica() {
         let cadena = "graph grafoDirigido{ rankdir=LR; node [shape=box]; \"/\"; node [shape = ellipse] ; layout=neato; "
         let auxPadre = this.principal
         let auxHijo = this.principal
         let peso = 0
-        while(auxPadre){
+        while (auxPadre) {
             auxHijo = auxPadre.siguiente
             let profundidad = auxPadre.valor.split('/')
             let padre = ""
-            if(profundidad.length == 2 && profundidad[1] == ""){ peso = 1}
-            else if(profundidad.length == 2 && profundidad[1] != ""){ peso = 2 }
+            if (profundidad.length == 2 && profundidad[1] == "") { peso = 1 }
+            else if (profundidad.length == 2 && profundidad[1] != "") { peso = 2 }
             else { peso = profundidad.length }
-            if(auxPadre.valor != "/"){ padre = profundidad[profundidad.length-1] }
+            if (auxPadre.valor != "/") { padre = profundidad[profundidad.length - 1] }
             else { padre = "/" }
-            while(auxHijo){
+            while (auxHijo) {
                 cadena += "\"" + padre + "\"" + " -- " + "\"" + auxHijo.valor + "\"" + " [label=\"" + peso + "\"] "
                 auxHijo = auxHijo.siguiente
             }
@@ -86,19 +114,125 @@ class grafoDirigido{
     }
 }
 
-const grafo =  new grafoDirigido()
-
-function insertar(){
-    let padre = document.getElementById("padre").value 
-    let hijos = document.getElementById("hijos").value 
-    grafo.insertarValores(padre,hijos)
-    //refrescarGrafo()
+function genera_tabla() {
+    let nombree = localStorage.getItem("nombrealumno");
+    let padre = localStorage.getItem(nombree + "padre");
+    let hijos = localStorage.getItem(nombree + "hijos");
+    let contador = localStorage.getItem(nombree + "contador");
+    let padre1 = padre.split(',');
+    let hijos1 = hijos.split(',');
+    let contador1 = contador.split(',');
+    var body = document.getElementsByTagName("body2")[0];
+    var tabla = document.createElement("table");
+    tabla.style.width = "300%"; // Ancho de la tabla
+    tabla.style.height = "200px"; // Altura de la tabla
+    var tblBody = document.createElement("tbody");
+    var hilera = document.createElement("tr");
+    var celda = document.createElement("td");
+    var textoCelda = document.createTextNode("Padre");
+    celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+    tblBody.appendChild(hilera);
+    var celda = document.createElement("td");
+    var textoCelda = document.createTextNode("Hijo");
+    celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+    tblBody.appendChild(hilera);
+    for (let i = 0; i < padre1.length; i++) {
+        const hijosCount = parseInt(contador1[i]);
+        const hijosStartIndex = i > 0 ? parseInt(contador1.slice(0, i).reduce((a, b) => parseInt(a) + parseInt(b))) : 0;
+        const hijosEndIndex = hijosStartIndex + hijosCount;
+      
+        var hilera = document.createElement("tr");
+        
+        var celda = document.createElement("td");
+        var textoCelda = document.createTextNode(padre1[i]);
+        celda.appendChild(textoCelda);
+        hilera.appendChild(celda);
+      
+        var celda = document.createElement("td");
+        for (let j = hijosStartIndex; j < hijosEndIndex; j++) {
+          var textoCelda = document.createTextNode(hijos1[j] + ", ");
+          celda.appendChild(textoCelda);
+        }
+        hilera.appendChild(celda);
+      
+        tblBody.appendChild(hilera);
+      }
+      
+    tabla.appendChild(tblBody);
+    body.appendChild(tabla);
+    tabla.setAttribute("border", "2");
+    tabla.style.borderCollapse = "collapse";
+    tabla.style.border = "solid black 1px";
+    tabla.style.fontSize = "20px";
+    tabla.style.textAlign = "center";
+    tblBody.style.height = "100px"; // Altura del tbody
+    hilera.style.height = "50px"; // Altura de las filas
+    celda.style.width = "150px"; // Ancho de las celdas
 }
 
-function refrescarGrafo(){
+
+const grafo = new grafoDirigido()
+integrarGraf();
+const subirPadre = [];
+const subirHijos = [];
+const subirContador =[];
+
+function integrarGraf() {
+    let nombree = localStorage.getItem("nombrealumno");
+    let padre = localStorage.getItem(nombree + "padre");
+    let hijos = localStorage.getItem(nombree + "hijos");
+    let contador = localStorage.getItem(nombree + "contador");
+    let padre1 = padre.split(',');
+    let hijos1 = hijos.split(',');
+    let contador1 = contador.split(',');
+    for (let i = 0; i < padre1.length; i++) {
+        const hijosCount = parseInt(contador1[i]);
+        const hijosStartIndex = i > 0 ? parseInt(contador1.slice(0, i).reduce((a, b) => parseInt(a) + parseInt(b))) : 0;
+        const hijosEndIndex = hijosStartIndex + hijosCount;
+        for (let j = hijosStartIndex; j < hijosEndIndex; j++) {
+            grafo.insertarValores(padre1[i],hijos1[j]);
+        }
+    }
+    genera_tabla();
+    refrescarGrafo();
+}
+
+function insertar() {
+    let padre = document.getElementById("padre").value;
+    let hijos = document.getElementById("hijos").value;
+    let nombree = localStorage.getItem("nombrealumno");
+    let subirHijos1 = [];
+    let auxiliar=hijos.split(',');
+    for(let i=0;i<auxiliar.length;i++){
+        subirHijos1.push(auxiliar[i]);
+    }
+    subirPadre.push(padre);
+    subirHijos.push(subirHijos1);
+    subirContador.push(subirHijos1.length);
+    localStorage.setItem(nombree + "padre", subirPadre);
+    localStorage.setItem(nombree + "hijos", subirHijos);
+    localStorage.setItem(nombree + "contador", subirContador);
+    grafo.insertarValores(padre, hijos);
+    genera_tabla();
+    refrescarGrafo();
+}
+
+
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", onChange, false);
+function onChange(event) {
+    let varr = event.target.files[0].name;
+    document.getElementById("hijos").value = varr;
+}
+
+
+function refrescarGrafo() {
     let url = 'https://quickchart.io/graphviz?graph=';
     let body = grafo.grafica()
     $("#image").attr("src", url + body);
     document.getElementById("padre").value = ""
     document.getElementById("hijos").value = ""
+    localStorage.setItem("grafo", body);
 }
--- index.js
+++ index.js
@@ -8,19 +8,18 @@ class login{
         this.btn.addEventListener('click', this.validar.bind(this));
     }
     validar() {
+      if(this.user.value === 'admin' && this.passw.value === 'admin'){
+        window.location = 'dashboard.html';
+      }
         const data = JSON.parse(localStorage.getItem("alumnos"));
-      
         if (!data) {
           alert('No hay datos guardados en el localStorage');
           return;
         }
-      
         console.log(JSON.stringify(data));
         const alumno = data.find((alumno) => alumno.carnet === parseInt(this.user.value) && alumno.contraseña === this.passw.value);
-      
-        if(this.user.value === 'admin' && this.passw.value === 'admin'){
-          window.location = 'dashboard.html';
-        }else if(alumno){
+        if(alumno){
+          localStorage.setItem("nombrealumno", JSON.stringify(this.user.value));
           window.location = 'dashboardestudiante.html';
         }else{
           alert('Usuario o contraseña incorrectos');
